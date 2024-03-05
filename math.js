208bbf5e61997081f8d60ecc032f10b781abab63
AngelValentino <138892399+AngelValentino@users.noreply.github.com>
2024-03-05 12:11:58 +0100

    Prime number.

--- math.js
+++ math.js
@@ -27,6 +27,26 @@ function factorial(n) {
 // O(n)
 /* END OF FACTORIAL */ 
 
+/* PRIME NUMBER */
+// If the number is composite; One of the two factors will be less tan or equal the square root for the given number(n = a * b), so we can stop the loop exection there as there is no need to go further because we should already have found a divisible number alredy. If not, it is prime.
+
+function isPrime(n) {
+  if (n < 2) {
+    return false;
+  }                      
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// O(sqrt(n))
+/* END OF PRIME NUMBER */
+
 btnLm.addEventListener('click', () => {
-  console.log(factorial(5));
+  console.log(isPrime(37));
+  console.log(isPrime(4));
+  console.log(isPrime(1));
 });
