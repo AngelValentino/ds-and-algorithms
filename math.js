757d3eb3246c1004b3bc9395e36d3f3a89988e52
AngelValentino <138892399+AngelValentino@users.noreply.github.com>
2024-03-12 11:12:53 +0100

    Power of two.

--- math.js
+++ math.js
@@ -2,6 +2,7 @@ const btnLm = document.querySelector('button');
 
 /* FIBONACCI SEQUENCE */
 
+// O(n)
 function fibonacci(n) {
   const fibonacciSequence = [0, 1];
   for (let i = 2; i < n; i++) {
@@ -10,11 +11,11 @@ function fibonacci(n) {
   return fibonacciSequence;
 }
 
-// O(n)
 /* END OF FIBONACCI SEQUENCE */
 
 /* FACTORIAL */
  
+// O(n)
 function factorial(n) {
   let result = 1;
   while(n > 0) {
@@ -24,12 +25,12 @@ function factorial(n) {
   return result;
 } 
 
-// O(n)
 /* END OF FACTORIAL */ 
 
 /* PRIME NUMBER */
 // If the number is composite; One of the two factors will be less tan or equal the square root for the given number(n = a * b), so we can stop the loop exection there as there is no need to go further because we should already have found a divisible number alredy. If not, it is prime.
 
+// O(sqrt(n))
 function isPrime(n) {
   if (n < 2) {
     return false;
@@ -42,11 +43,46 @@ function isPrime(n) {
   return true;
 }
 
-// O(sqrt(n))
 /* END OF PRIME NUMBER */
 
+/* POWER OF TWO*/
+
+// O(logN)
+function isPowerOfTwo(n) {
+  if (n < 1) {
+    return false;
+  }
+  while(n > 1) {
+    if (n % 2 !== 0) {
+      return false;
+    }
+    n /= 2;
+  }
+  return true;
+} 
+
+// O(1)
+function isPowerOfTwoBitWise(n) {
+  if (n <= 0) {
+    return false;
+  }
+  return (n & (n - 1)) === 0; 
+}
+
+// isPowerOfTwoBitWise(8) => 
+//       8 === 1000
+//       7 === 0111
+//   8 & 7 === 0000
+
+
+/* END OF POWER OF TWO */
+
 btnLm.addEventListener('click', () => {
-  console.log(isPrime(37));
-  console.log(isPrime(4));
-  console.log(isPrime(1));
+  console.log(isPowerOfTwoBitWise(0));
+  console.log(isPowerOfTwoBitWise(1));
+  console.log(isPowerOfTwoBitWise(2));
+  console.log(isPowerOfTwoBitWise(2)); //true
+  console.log(isPowerOfTwoBitWise(5)); //false
+  console.log(isPowerOfTwoBitWise(8));
+  console.log(isPowerOfTwoBitWise(100)); //false
 });
