fece4c32e071ec4fca25218a9fb90a8d8dc7bdb1
AngelValentino <138892399+AngelValentino@users.noreply.github.com>
2024-03-14 13:44:00 +0100

    Fixed binary search and added binary search recursive.

--- search.js
+++ search.js
@@ -24,8 +24,8 @@ function binarySearch(arr, target) {
     const mid = Math.floor((start + end) / 2)
     if (arr[mid] === target) {
       return mid;
-    }
-    if (mid < target) {
+    } 
+    else if (arr[mid] < target) {
       start = mid + 1;
     } 
     else {
@@ -36,9 +36,39 @@ function binarySearch(arr, target) {
   return -1;
 }
 
+function binarySearchRecursive(arr, target, start = 0, end = arr.length - 1) {
+  if (start > end) {
+    return -1;
+  }
+  const mid = Math.floor((start + end) / 2);
+  if (arr[mid] === target) {
+    return mid;
+  } 
+  else if (arr[mid] < target) {
+    // start = mid + 1
+    return binarySearchRecursive(arr, target, mid + 1, end);
+  } 
+  else {
+    // end = mid - 1
+    return binarySearchRecursive(arr, target, start, mid - 1);
+  }
+}
+
+// binarySearchRecursive([0, 2, 5, 9, 10, 11, 28, 65, 85, 99], 85);
+// start = 0;
+// end = 9;
+// mid = 4
+//   return binarySearchRecursive(arr, target, 5, 9)
+//   mid = 7;
+//     return binarySearchRecursive(arr, target, 8, 9);
+//     mid = 8; 
+//     return 8;
+
 /* END OF BINARY SEARCH */
 
 
 btnLm.addEventListener('click', () => {
-  console.log(binarySearch([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 8));
+  console.log(binarySearchRecursive([0, 2, 5, 9, 10, 11, 28, 65, 85, 99], 85));
 });
+
+
