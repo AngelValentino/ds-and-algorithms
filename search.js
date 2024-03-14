9bd3f2ac7e91e539460c27093b2c96c8b31d65ab
AngelValentino <138892399+AngelValentino@users.noreply.github.com>
2024-03-14 11:12:32 +0100

    Binary search.

--- search.js
+++ search.js
@@ -1,9 +1,9 @@
 // O(n)
 /* LINEAR SEARCH */
 
-function linearSearch(array, target) {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === target) {
+function linearSearch(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
       return [i];
     }
   }
@@ -12,7 +12,33 @@ function linearSearch(array, target) {
 
 /* END OF LINEAR SEARCH */
 
+// The array needs to be sorted in order to use binary search.
+// O(log n)
+/* BINARY SEARCH */
+
+function binarySearch(arr, target) {
+  let start = 0;
+  let end = arr.length - 1;
+
+  while (start <= end) {
+    const mid = Math.floor((start + end) / 2)
+    if (arr[mid] === target) {
+      return mid;
+    }
+    if (mid < target) {
+      start = mid + 1;
+    } 
+    else {
+      end = mid - 1;
+    }
+  }
+
+  return -1;
+}
+
+/* END OF BINARY SEARCH */
+
 
 btnLm.addEventListener('click', () => {
-  console.log(linearSearch([1, 2, 6, 34], 6));
+  console.log(binarySearch([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 8));
 });
