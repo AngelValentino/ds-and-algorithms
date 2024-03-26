0676dabd66599405e8374fe77e6df0fc020d1f0f
AngelValentino <138892399+AngelValentino@users.noreply.github.com>
2024-03-26 11:43:55 +0100

    Check if the word is a palindrome.

--- misc.js
+++ misc.js
@@ -14,6 +14,20 @@ const reverseString2 = (str) => str.split('').reverse().join('');
 
 /* END OF REVERSE A STRING */
 
+/* PALINDROME */
+
+//O(n)
+function isPalindrome(str) {
+  const formatedStr = str.replace(/\s+/g, '');
+  let reversedStr = '';
+  for (char of formatedStr) {
+    reversedStr = char + reversedStr; 
+  }
+  return reversedStr === formatedStr;
+}
+
+/* END OF PALINDROME */
+
 btnLm.addEventListener('click', () => {
-  console.log(reverseString('banana'));
+  console.log(isPalindrome('race car'));
 });
