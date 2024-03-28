4d06fca58ca1cb354bc5caf7c943b74101002a3d
AngelValentino <138892399+AngelValentino@users.noreply.github.com>
2024-03-28 12:43:42 +0100

    FizzBuzz.

--- misc.js
+++ misc.js
@@ -103,6 +103,44 @@ function reverseNum(num) {
 
 /* END OF REVERSE AN NUMBER */
 
+/* FIZZBUZZ */
+
+//O(n)
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    if (!(i % 15)) {
+      console.log('fizzBuzz');
+    } 
+    else if (!(i % 5)) {
+      console.log('buzz');
+    } 
+    else if (!(i % 3)) {
+      console.log('fizz');
+    } 
+    else {
+      console.log(i);
+    }
+  }
+}
+
+//O(n)
+function fizzBuzz2(n) {
+  for (let i = 1; i <= n; i++) {
+    let output = '';
+
+    if (!(i % 3)) {
+      output += 'fizz';
+    }
+    if (!(i % 5)) {
+      output += 'buzz';
+    }
+
+    output ? console.log(output) : console.log(i); 
+  }
+}
+
+/* END OF FIZZBUZ */
+
 btnLm.addEventListener('click', () => {
-  console.log(reverseNum(2.587));
+  fizzBuzz2(150);
 });
