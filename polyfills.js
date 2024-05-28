3e5fc5c253400e30dd3c608790f81459559613cc
AngelValentino <138892399+AngelValentino@users.noreply.github.com>
2024-05-28 11:55:12 +0200

    Added Array find and findIndex polyfills.

--- polyfills.js
+++ polyfills.js
@@ -14,6 +14,28 @@ Array.prototype.customFilter = function(callback) {
 
 //? END OF ARRAY FILTER
 
+//? ARRAY ARRAY FIND AND FIND INDEX
+
+Array.prototype.customFind = function(callback) {
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this)) {
+      return this[i];
+    }
+  }
+  return;
+}
+
+Array.prototype.customFindIndex = function(callback) {
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this)) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+//? END OF ARRAY FIND AND FIND INDEX
+
 btnLm.addEventListener('click', () => {
-  console.log([1, 2, 3, 4, 5, 6].customFilter((num) => num > 2 ));
+  console.log([1, 2, 3, 4, 5, 6].customFind((num) => num > 20));
 });
